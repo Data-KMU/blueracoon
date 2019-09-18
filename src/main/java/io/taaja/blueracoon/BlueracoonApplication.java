0f2d98f2d33d5f0819c47e63de1791e6ac6ddc7d
Kris <kris.raich@gmail.com>
2019-09-18 08:54:31 +0200

    Initial commit

---
+++ src/main/java/io/taaja/blueracoon/BlueracoonApplication.java
@@ -0,0 +1,13 @@
+package io.taaja.blueracoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueracoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueracoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/taaja/blueracoon/BlueracoonApplicationTests.java
@@ -0,0 +1,16 @@
+package io.taaja.blueracoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueracoonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
