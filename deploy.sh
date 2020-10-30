336a2365ae029c05612905b360a6b4fab6795406
kris <kris.raich@gmail.com>
2020-10-30 15:07:10 +0100

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker build -f src/main/docker/Dockerfile.jvm -t taaja/blueracoon . && \
 docker save -o taaja-blueracoon.tar taaja/blueracoon && \
 scp taaja-blueracoon.tar taaja@taaja.io:/home/taaja && \
-ssh taaja@taaja.io 'docker load --quiet --input /home/taaja/taaja-blueracoon.tar && cd /home/taaja/deployment/taaja && docker-compose up -d'
+ssh taaja@taaja.io 'docker load --quiet --input /home/taaja/taaja-blueracoon.tar  && rm -f /home/taaja/taaja-blueracoon.tar && cd /home/taaja/deployment/taaja && docker-compose up -d'
